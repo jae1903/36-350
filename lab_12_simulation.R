7609e77559c0b2599691f17f152abe6b51857f77
jaewony <38471311+jaewony@users.noreply.github.com>
2018-04-18 13:14:41 -0400

    Delete lab_12_simulation.R

--- lab_12_simulation.R
+++
@@ -1,7 +0,0 @@
-generate_data = function(n,p) {
-  covariates = matrix(rnorm(n), nrow = n, ncol = p)
-  responses = rnorm(n)
-  return(list(covariates = covariates, responses = responses))
-}
-
-generate_data(3, 5)

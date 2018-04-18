f5a20f02fc1d20b70a96b5ce227a9e7acfc09743
jaewony <38471311+jaewony@users.noreply.github.com>
2018-04-18 14:22:55 -0400

    add part b

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,14 @@ generate_data = function(n,p) {
   return(list(covariates = covariates, responses = responses))
 }
 
+model_select = function(covariates, responses, cutoff) {
+  lm.res.cov = lm(responses ~ covariates)
+  p.value = (summary(lm.res.cov)$coefficients)[-1, 4]
+  na.vals = which(p.value <= cutoff)
+  if (length(na.vals) == 0) {
+    return(c())
+  }
+  lm.res.cov.2 = lm(responses ~ covariates[, na.vals])
+  p.value.2 = (summary(lm.res.cov.2)$coefficients)[-1, 4]
+  return(p.value.2)
+}
